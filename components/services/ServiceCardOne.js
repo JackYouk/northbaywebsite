2cb106d042f6de402e3ac317d88d2c63b3d74724
JackYouk <jackyoukstetter1@gmail.com>
2022-10-27 22:17:40 -0700

    fixes

--- components/services/ServiceCardOne.js
+++ components/services/ServiceCardOne.js
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import {useEffect, useState} from 'react';
 import Tilt from 'react-parallax-tilt';
-import {camelCaseToDashed} from '../../helpers/utilities';
 
 const ServiceCardOne = (
     {
